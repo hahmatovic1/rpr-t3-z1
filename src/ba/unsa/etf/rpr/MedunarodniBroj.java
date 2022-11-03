5bde4c5cb9f79a4e07f341e77d94c9d95c4a8a8c
hahmatovic1 <hahmatovic1@etf.unsa.ba>
2022-11-03 18:04:18 +0100

    Kreirana klasa MedunarodniBroj sa konstruktorom, geterima i seterima

---
+++ src/ba/unsa/etf/rpr/MedunarodniBroj.java
@@ -0,0 +1,47 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Objects;
+
+public class MedunarodniBroj extends TelefonskiImenik{
+    private String drzava;
+    private String broj;
+
+    public MedunarodniBroj(String drzava, String broj) {
+        this.drzava = drzava;
+        this.broj = broj;
+    }
+
+    public String getDrzava() {
+        return drzava;
+    }
+
+    public void setDrzava(String drzava) {
+        this.drzava = drzava;
+    }
+
+    public String getBroj() {
+        return broj;
+    }
+
+    public void setBroj(String broj) {
+        this.broj = broj;
+    }
+
+    @Override
+    public String Ispisi() {
+        return drzava + broj;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MedunarodniBroj that = (MedunarodniBroj) o;
+        return drzava.equals(that.drzava) && broj.equals(that.broj);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(drzava, broj);
+    }
+}
