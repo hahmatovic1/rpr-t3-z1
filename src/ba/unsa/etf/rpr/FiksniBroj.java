abb4045131be1a61f3197a7f1f5bfaa230f7b59c
hahmatovic1 <hahmatovic1@etf.unsa.ba>
2022-11-03 17:45:45 +0100

    Kreirana klasa Fiksni broj

---
+++ src/ba/unsa/etf/rpr/FiksniBroj.java
@@ -0,0 +1,48 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Objects;
+
+public class FiksniBroj extends   TelefonskiImenik{
+
+    private String broj;
+    private Grad grad;
+
+    public FiksniBroj(String broj, Grad grad) {
+        this.broj = broj;
+        this.grad = grad;
+    }
+
+    public String getBroj() {
+        return broj;
+    }
+
+    public void setBroj(String broj) {
+        this.broj = broj;
+    }
+
+    public Grad getGrad() {
+        return grad;
+    }
+
+    public void setGrad(Grad grad) {
+        this.grad = grad;
+    }
+
+    @Override
+    public String Ispisi() {
+        return this.getGrad()+"/"+getBroj();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FiksniBroj that = (FiksniBroj) o;
+        return broj.equals(that.broj) && grad == that.grad;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(broj, grad);
+    }
+}
